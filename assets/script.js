8072c160673c65ed36f7f4bf35dddbafdfe8aa3d
Harsh Vardhan <harshvardhan232002@gmail.com>
2024-09-01 17:32:57 +0530

    Awesome!

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,31 @@
-const sidebar = document.getElementById('sidebar');
-const menuToggle = document.getElementById('menuToggle');
+// For aside bar-
+const sidebar = document.getElementById('sidebar'); //selects sidebar
+const menuToggle = document.getElementById('menuToggle'); //selects menu button
 
 menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
+    sidebar.classList.toggle('collapsed');  //classlist.toggle- it is a predefined property which which is applied on collapsed (what doed toggle do it adds a class if it is not present and removes a class if it is present).
     if (sidebar.classList.contains('collapsed')) {
-        menuToggle.textContent = '☰'; // Show menu icon when collapsed
-        menuToggle.style.marginTop = '10px'; // Adjust the margin to be close to the "Menu" heading
+        menuToggle.textContent = '☰';
+        menuToggle.style.marginTop = '10px';
     } else {
-        menuToggle.textContent = '✖'; // Show cross icon when expanded
-        menuToggle.style.marginTop = '20px'; // Reset margin
+        menuToggle.textContent = '✖';
+        menuToggle.style.marginTop = '20px';
     }
 });
+
+// For Time greeting-
+window.onload = function() {
+    const greetingElement = document.getElementById('greeting');
+    const currentHour = new Date().getHours();
+    let greetingMessage = '';
+
+    if (currentHour < 12) {
+        greetingMessage = 'Good Morning!';
+    } else if (currentHour < 18) {
+        greetingMessage = 'Good Afternoon!';
+    } else {
+        greetingMessage = 'Good Evening!';
+    }
+
+    greetingElement.textContent = greetingMessage;
+};
